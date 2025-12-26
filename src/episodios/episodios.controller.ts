623a76d209407fb2b6ec74f6b547c5582966dee6
minorinzzz <saraiblancosalgado2014@gmail.com>
2025-12-25 22:03:15 -0400

    Add JwtAuthGuard to protect POST, PATCH and DELETE routes

--- src/episodios/episodios.controller.ts
+++ src/episodios/episodios.controller.ts
@@ -6,16 +6,19 @@ import {
   Patch,
   Param,
   Delete,
+  UseGuards,
 } from '@nestjs/common';
 import { EpisodiosService } from './episodios.service';
 import { CreateEpisodioDto } from './dto/create-episodio.dto';
 import { UpdateEpisodioDto } from './dto/update-episodio.dto';
+import { JwtAuthGuard } from '../auth/guards/jwt-auth.guard';
 
 @Controller('episodios')
 export class EpisodiosController {
   constructor(private readonly episodiosService: EpisodiosService) {}
 
   @Post()
+  @UseGuards(JwtAuthGuard)
   create(@Body() createEpisodioDto: CreateEpisodioDto) {
     return this.episodiosService.create(createEpisodioDto);
   }
@@ -31,6 +34,7 @@ export class EpisodiosController {
   }
 
   @Patch(':id')
+  @UseGuards(JwtAuthGuard)
   update(
     @Param('id') id: string,
     @Body() updateEpisodioDto: UpdateEpisodioDto,
@@ -39,6 +43,7 @@ export class EpisodiosController {
   }
 
   @Delete(':id')
+  @UseGuards(JwtAuthGuard)
   remove(@Param('id') id: string) {
     return this.episodiosService.remove(+id);
   }
--- src/series/series.controller.ts
+++ src/series/series.controller.ts
@@ -6,16 +6,19 @@ import {
   Patch,
   Param,
   Delete,
+  UseGuards,
 } from '@nestjs/common';
 import { SeriesService } from './series.service';
 import { CreateSerieDto } from './dto/create-serie.dto';
 import { UpdateSerieDto } from './dto/update-serie.dto';
+import { JwtAuthGuard } from '../auth/guards/jwt-auth.guard';
 
 @Controller('series')
 export class SeriesController {
   constructor(private readonly seriesService: SeriesService) {}
 
   @Post()
+  @UseGuards(JwtAuthGuard)
   create(@Body() createSerieDto: CreateSerieDto) {
     return this.seriesService.create(createSerieDto);
   }
@@ -31,11 +34,13 @@ export class SeriesController {
   }
 
   @Patch(':id')
+  @UseGuards(JwtAuthGuard)
   update(@Param('id') id: string, @Body() updateSerieDto: UpdateSerieDto) {
     return this.seriesService.update(+id, updateSerieDto);
   }
 
   @Delete(':id')
+  @UseGuards(JwtAuthGuard)
   remove(@Param('id') id: string) {
     return this.seriesService.remove(+id);
   }
